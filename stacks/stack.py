53f38cb9dfa202d24e936844326819f544fb15f4
Bartłomiej Bęczkowski <beczkowb@gmail.com>
2017-03-13 22:51:24 +0100

    stack

---
+++ stacks/stack.py
@@ -0,0 +1,66 @@
+import unittest
+
+
+class Node:
+    def __init__(self, data, prev=None):
+        self.prev = prev
+        self.data = data
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        if self.top is None:
+            self.top = Node(data)
+        else:
+            self.top = Node(data, prev=self.top)
+
+    def pop(self):
+        if self.top:
+            prev_top = self.top
+            self.top = prev_top.prev
+            return prev_top.data
+        else:
+            return None
+
+    def peek(self):
+        if self.top:
+            return self.top.data
+        else:
+            return None
+
+    def is_empty(self):
+        return self.peek() is None
+
+
+class StackTestCase(unittest.TestCase):
+    def test(self):
+        stack = Stack()
+        self.assertIsNone(stack.peek())
+        self.assertIsNone(stack.pop())
+        self.assertTrue(stack.is_empty())
+
+        stack.push(1)
+        self.assertEqual(stack.peek(), 1)
+        self.assertFalse(stack.is_empty())
+        self.assertEqual(stack.pop(), 1)
+
+        self.assertIsNone(stack.peek())
+        self.assertIsNone(stack.pop())
+        self.assertTrue(stack.is_empty())
+
+        stack.push(1)
+        stack.push(2)
+        self.assertEqual(stack.peek(), 2)
+        self.assertFalse(stack.is_empty())
+        self.assertEqual(stack.pop(), 2)
+
+        self.assertEqual(stack.peek(), 1)
+        self.assertFalse(stack.is_empty())
+        self.assertEqual(stack.pop(), 1)
+
+        self.assertIsNone(stack.peek())
+        self.assertIsNone(stack.pop())
+        self.assertTrue(stack.is_empty())
